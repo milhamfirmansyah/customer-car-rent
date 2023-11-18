c73177033d9830727700907734a729ed2fd0aee5
Ilham Firmansyah <milhamfirmansyah5@gmail.com>
2023-11-18 19:55:17 +0700

    removed all components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import Image from 'next/image'
-import Navbar from './components/Navbar'
-import Reason from './components/Reason'
-import Hero from './components/Hero'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      {/* <Reason /> */}
+      <h1>Hello World</h1>
     </div>
   )
 }
