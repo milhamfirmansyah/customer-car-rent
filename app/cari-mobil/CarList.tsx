76913de2a8af6d8f326a20491245ef1bc219465c
Ilham Firmansyah <milhamfirmansyah5@gmail.com>
2023-12-17 10:40:37 +0700

    added logic category input

--- app/cari-mobil/CarList.tsx
+++ app/cari-mobil/CarList.tsx
@@ -6,9 +6,11 @@ import FilterCar from './FilterCar';
 
 function CarList() {
   const [name, setName] = useState('');
+  const [category, setCategory] = useState('')
+  const [price, setPrice] = useState('')
 
   const fetcher = async () => {
-    const res = await axios.get(`https://api-car-rental.binaracademy.org/customer/v2/car?name=${name}`);
+    const res = await axios.get(`https://api-car-rental.binaracademy.org/customer/v2/car?name=${name}&category=${category}&price=${price}`);
     return res.data.cars;
   };
 
@@ -22,13 +24,21 @@ function CarList() {
     setName(e.target.value);
   };
 
-  const handleClick = ()=>{
-    mutate()
+  const handleChangeCategory = (e: any)=>{
+    setCategory(e.target.value)
   }
+
+  const handleChangePrice = (e: any)=>{
+    setPrice(e.target.value)
+  }
+
+  const handleClick = () => {
+    mutate();
+  };
 
   return (
     <>
-      <FilterCar name={handleChangeName} click={()=>handleClick()} />
+      <FilterCar name={handleChangeName} category={handleChangeCategory} price={handleChangePrice} click={handleClick} />
       <div className="max-w-[1047px] mx-auto flex flex-wrap justify-between gap-6">
         {data.map((item: any) => (
           <div className="w-[333px] p-6 shadow-list-car rounded-lg">
--- app/cari-mobil/FilterCar.tsx
+++ app/cari-mobil/FilterCar.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function FilterCar({ name, click }: any) {
+function FilterCar({ name, category, price, click }: any) {
+
   return (
     <div className="w-[1047px] h-[106px] bg-[#FFF] shadow-md mx-auto rounded-lg translate-y-[-40px] pl-[28px] pr-[20px] pt-[17px] pb-[31px] flex justify-between items-end">
       {/* Nama Mobil */}
@@ -14,7 +15,7 @@ function FilterCar({ name, click }: any) {
           id="name"
           onChange={name}
           placeholder="ketik nama/tipe mobil"
-          className="w-[208px] h-9 py-[9px] px-3 outline-none border border-[#CCC] rounded-sm placeholder:text-xs placeholder:leading-[18px] placeholder:font-light text-xs"
+          className="w-[208px] h-9 py-[9px] px-3 outline-none border border-[#CCC] rounded-sm placeholder:text-xs placeholder:leading-[18px] placeholder:font-light placeholder:text-[#8A8A8A] text-xs"
         />
       </div>
 
@@ -24,12 +25,12 @@ function FilterCar({ name, click }: any) {
           Kategori
         </label>
         <br />
-        <input
-          type="search"
-          id="kategori"
-          placeholder="Masukkan Kapasitas Mobil"
-          className="w-[208px] h-9 py-[9px] px-3 outline-none border border-[#CCC] rounded-sm placeholder:text-xs placeholder:leading-[18px] placeholder:font-light text-xs"
-        />
+        <select name="" id="kategori" onChange={category} className="w-[208px] h-9 py-[9px] px-3 outline-none border border-[#CCC] rounded-sm text-xs text-[#8A8A8A]">
+          <option value="">Masukan Kapasitas Mobil</option>
+          <option value="small">2 - 4 orang</option>
+          <option value="medium">4 - 6 orang</option>
+          <option value="large">6 - 8 orang</option>
+        </select>
       </div>
 
       {/* Harga */}
@@ -38,12 +39,12 @@ function FilterCar({ name, click }: any) {
           Harga
         </label>
         <br />
-        <input
-          type="search"
-          id="harga"
-          placeholder="Masukkan Harga Sewa per Hari"
-          className="w-[219px] h-9 py-[9px] px-3 outline-none border border-[#CCC] rounded-sm placeholder:text-xs placeholder:leading-[18px] placeholder:font-light text-xs"
-        />
+        <select name="" id="harga" onChange={price} className="w-[208px] h-9 py-[9px] px-3 outline-none border border-[#CCC] rounded-sm text-xs text-[#8A8A8A]">
+          <option value="">Masukan Harga Sewa per Hari</option>
+          <option value="">&lt; Rp. 400.000</option>
+          <option value="">Rp. 400.000 - Rp. 600.000</option>
+          <option value="">&gt; Rp. 600.000</option>
+        </select>
       </div>
 
       {/* Status */}
@@ -52,9 +53,16 @@ function FilterCar({ name, click }: any) {
           Status
         </label>
         <br />
-        <input type="search" id="status" placeholder="status" className="w-[208px] h-9 py-[9px] px-3 outline-none border border-[#CCC] rounded-sm placeholder:text-xs placeholder:leading-[18px] placeholder:font-light text-xs" />
+        <input
+          type="search"
+          id="status"
+          placeholder="status"
+          className="w-[208px] h-9 py-[9px] px-3 outline-none border border-[#CCC] rounded-sm placeholder:text-xs placeholder:leading-[18px] placeholder:font-light placeholder:text-[#8A8A8A] text-xs"
+        />
       </div>
-      <button onClick={click} className="w-[92px] h-9 bg-[#5CB85F] rounded-sm text-sm text-[#FFF] font-bold">Cari</button>
+      <button onClick={click} className="w-[92px] h-9 bg-[#5CB85F] rounded-sm text-sm text-[#FFF] font-bold">
+        Cari
+      </button>
     </div>
   );
 }
